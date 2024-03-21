8eb0a11bd50c68242f9c5bf6bff1a6c427a875d2
hoangkhacphuc <hoangkhacphuc.dev@gmail.com>
2024-03-21 14:46:01 +0700

    fix: lang folder

--- src/ValidationServiceProvider.php
+++ src/ValidationServiceProvider.php
@@ -1,17 +1,27 @@
 <?php
 
 namespace Sphoton\Validation;
+
 use Illuminate\Support\ServiceProvider;
 
 class ValidationServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'sphoton');
+        $this->loadTranslationsFrom(__DIR__.'/../lang', 'sphoton');
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/lang' => $this->app->resourcePath('lang/vendor/sphoton'),
+                __DIR__.'/../lang' => $this->langPath('vendor/sphoton'),
             ]);
         }
+    }
+
+    protected function langPath(mixed $path = ''): string
+    {
+        if (method_exists($this->app, 'langPath')) {
+            return $this->app->langPath($path);
+        }
+
+        return $this->app->resourcePath('lang/'.$path);
     }
 }
